22a03cafef5554e0a8002689b2174ad74c10c5de
John Gannon <johgannon@gmail.com>
2020-03-30 16:48:06 +0100

    Add stubs for Vote.vue and its sub components

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This will be the game view</h1>
+    <Vote>This will be the game view</Vote>
   </div>
 </template>
+
+<script>
+import Vote from '@/components/Vote.vue'
+export default {
+  name: 'Game',
+  props: {
+    msg: String
+  },
+  components: {
+    Vote
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
