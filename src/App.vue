8104e42abd4469ad980cff566ec1fb730027cae7
John Gannon <johgannon@gmail.com>
2020-03-30 18:23:36 +0100

    Merge master

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,7 @@
   </div>
 </template>
 
-<style scoped>
-@import "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css";
-</style>
+<style scoped></style>
 
 <style lang="scss">
 #app {
