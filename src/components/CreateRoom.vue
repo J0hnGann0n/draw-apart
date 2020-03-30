a3eec2b197665c22bc92d0ed556e8fad9619f512
Mirjam Thomet <mirjamthomet@gmail.com>
2020-03-30 16:37:27 +0100

    add component files

---
+++ src/components/CreateRoom.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <a type="button" class="btn btn-primary" href="lobby.html">Create a game</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CreateRoom"
+};
+</script>
---
+++ src/components/UserNameInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row">
+    <div class="input-group mb-3 col">
+      <input
+        type="text"
+        class="form-control"
+        id="username"
+        placeholder="Choose your username"
+        aria-describedby="emailHelp"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserNameInput"
+};
+</script>
