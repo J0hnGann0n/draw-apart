df1f67661f86133b835ba925e99888eab15517be
John Gannon <johgannon@gmail.com>
2020-03-30 17:03:26 +0100

    Merge branches and add baasic logic to select game component

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,12 +1,23 @@
 <template>
-  <Lobby />
+  <div>
+    <Lobby v-if="gameState == 'lobby'" />
+    <Vote v-if="gameState == 'vote'" />
+  </div>
 </template>
 <script>
 import Lobby from "@/components/Lobby.vue";
+import Vote from "@/components/Vote.vue";
+
 export default {
   name: "Game",
   components: {
-    Lobby
+    Lobby,
+    Vote
+  },
+  data: function() {
+    return {
+      gameState: "lobby"
+    };
   }
 };
 </script>
