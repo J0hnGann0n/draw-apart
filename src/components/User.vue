aa8611b9ff66d93c51dec7684bd12f752ac37c46
Mirjam Thomet <mirjamthomet@gmail.com>
2020-03-30 16:59:25 +0100

    add User component file

---
+++ src/components/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col-2">
+      <i class="fas fa-user-tie"></i>
+    </div>
+    <div class="col-10">
+      <p>{{user.name}}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "User",
+  props: ["user"]
+};
+</script>
