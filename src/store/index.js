943974d37efc0ffb4fc727c2c0bf51bd8b48ede5
Mirjam Thomet <mirjamthomet@gmail.com>
2020-03-31 20:06:23 +0100

    set up basic vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    game: {
+      users: {},
+      code: ''
+    }
   },
   mutations: {
+    ADD_USER(state, payload) {
+      let newUser = payload;
+      state.game.users.push(newUser);
+
+    }
   },
   actions: {
+    addUser(context, payload) {
+      context.commit('ADD_USER', payload);
+    }
   },
   modules: {
   }
