4ea0e71fdae7c9a357d80b2849626c0e46313bb6
John Gannon <johgannon@gmail.com>
2020-04-02 16:37:17 +0100

    Add basic drawing functionality

--- src/components/DrawingPanel.vue
+++ src/components/DrawingPanel.vue
@@ -7,7 +7,14 @@
     </div>
     <div class="row">
       <div class="col">
-        <p>Here comes the drawing panels</p>
+        <canvas
+          id="canvas"
+          v-on:mousedown="handleMouseDown"
+          v-on:mouseup="handleMouseUp"
+          v-on:mousemove="handleMouseMove"
+          width="800px"
+          height="800px"
+        ></canvas>
       </div>
     </div>
     <div class="col-10">
@@ -32,10 +39,31 @@ export default {
   data: function() {
     return {
       drawingCount: 0,
-      bodyPart: ["head", "body", "legs", "feet"]
+      bodyPart: ["head", "body", "legs", "feet"],
+      mouse: {
+        current: {
+          x: 0,
+          y: 0
+        },
+        previous: {
+          x: 0,
+          y: 0
+        },
+        down: false
+      }
     };
   },
-  components: {},
+  computed: {
+    currentMouse: function() {
+      var c = document.getElementById("canvas");
+      var rect = c.getBoundingClientRect();
+
+      return {
+        x: this.mouse.current.x - rect.left,
+        y: this.mouse.current.y - rect.top
+      };
+    }
+  },
   methods: {
     /**
      * Get canvas image as base64 and send drawing object to store to be submitted.
@@ -48,7 +76,52 @@ export default {
       };
       this.$store.dispatch("submitDrawing", drawing);
       this.drawingCount++;
+    },
+    draw: function() {
+      // requestAnimationFrame(this.draw);
+      if (this.mouse.down) {
+        var c = document.getElementById("canvas");
+
+        var ctx = c.getContext("2d");
+
+        ctx.clearRect(0, 0, 800, 800);
+
+        ctx.lineTo(this.currentMouse.x, this.currentMouse.y);
+        ctx.strokeStyle = "#F63E02";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+      }
+    },
+    handleMouseDown: function(event) {
+      this.mouse.down = true;
+      this.mouse.current = {
+        x: event.pageX,
+        y: event.pageY
+      };
+
+      var c = document.getElementById("canvas");
+      var ctx = c.getContext("2d");
+
+      ctx.moveTo(this.currentMouse.x, this.currentMouse.y);
+    },
+    handleMouseUp: function() {
+      this.mouse.down = false;
+    },
+    handleMouseMove: function(event) {
+      this.mouse.current = {
+        x: event.pageX,
+        y: event.pageY
+      };
+
+      this.draw(event);
     }
+  },
+  ready: function() {
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
+    ctx.translate(0.5, 0.5);
+    ctx.imageSmoothingEnabled = false;
+    // this.draw();
   }
 };
 </script>
