2ac65e53ed4a8b4f4163406ae0f2f3dec2d74147
John Gannon <johgannon@gmail.com>
2020-04-02 17:04:07 +0100

    Fix cors issue in firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,17 @@
 var async = require('async');
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 admin.initializeApp();
 
 exports.createGame = functions.https.onRequest((request, response) => {
-  response.set('Access-Control-Allow-Origin', '*');
-  let game = {
-    players: []
-  }
-  console.log(request)
-  game.players.push(request.body.name)
-  admin.database().ref('/games/').push(game)
-  response.send(`Hello from Firebase! ${JSON.stringify(request.body)}`);
+  cors(request, response, () => {
+    let game = {
+      players: []
+    }
+    console.log(request)
+    game.players.push(request.body.name)
+    admin.database().ref('/games/').push(game)
+    response.send(`Hello from Firebase! ${JSON.stringify(request.body)}`);
+  });
 })
