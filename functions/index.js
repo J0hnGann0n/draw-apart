886c292bdceae7bb06f4afcd66d1fa326a8ab26c
Mirjam Thomet <mirjamthomet@gmail.com>
2020-04-04 13:05:13 +0100

    fix create game

--- functions/index.js
+++ functions/index.js
@@ -23,7 +23,7 @@ function generateCode(length) {
  * Checks if Code is unique
  * @param {*} code 
  */
-async function isCodeUniqe(code) {
+async function codeExists(code) {
   let checkDB = await ref.child('games').orderByChild('code').equalTo(code).once('value').then(snapshot => {
     if (snapshot.exists()) {
       return snapshot.val();
@@ -31,7 +31,6 @@ async function isCodeUniqe(code) {
       return false;
     }
   }).catch(error => {
-    response.send("Etwas ist schief gelaufen")
     return error
   })
   return checkDB;
@@ -49,30 +48,14 @@ exports.createGame = functions.https.onRequest((request, response) => {
       drawings: [],
       combinations: []
     }
-    let uniqeGameCode = false;
-    let gameCode = "";
-
     //Generate game code
-
-    gameCode = generateCode(4);
-    while (uniqeGameCode === false) {
-      if (isCodeUniqe(gameCode)) {
-        gameCode = generateCode(4)
-      } else {
-        uniqeGameCode = true;
-      }
-    }
-
-    if (uniqeGameCode) {
-      //add game code
-      game.code = gameCode
-      //add provided playername to game object
-      game.players[request.body.name] = true;
-      //push game object to games in db
-      let createGameInDB = admin.database().ref('/games/').push(game)
-      //return game object
-      response.send(createGameInDB.key);
-    }
+    game.code = generateCode(4);
+    //add provided playername to game object
+    game.players[request.body.name] = true;
+    //push game object to games in db
+    let createGameInDB = admin.database().ref('/games/').push(game)
+    //return game object
+    response.send(createGameInDB.key);
 
   });
 })
@@ -85,7 +68,7 @@ exports.joinGame = functions.https.onRequest((request, response) => {
     let gameCode = request.body.gamecode
     let player = request.body.player
 
-    isCodeUniqe(gameCode).then(res => {
+    codeExists(gameCode).then(res => {
       let gameKey = Object.keys(res)[0]
       //ref.child('games/' + gameKey + '/players').child(player).setValue(true);
       response.send(gameKey)
