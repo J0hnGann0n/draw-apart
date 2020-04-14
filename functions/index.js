bbb9d609cd448d589d3861743099bea37dab0395
John Gannon <johgannon@gmail.com>
2020-04-14 20:24:16 +0200

    cleaup

--- functions/index.js
+++ functions/index.js
@@ -96,9 +96,6 @@ exports.setState = functions.database.ref('/games/{gameKey}')
       if (players === playersFinishedDrawing) {
         ref.child('games/' + gameKey).child("state").set("combination");
       }
-      // You must return a Promise when performing asynchronous tasks inside a Functions such as
-      // writing to the Firebase Realtime Database.
-      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-      // return snapshot.ref.parent.child('uppercase').set(uppercase);
+
       return true
     });
