9a7efea20adb6ada652f9cdb489c9d5a5e83537b
mirjam <mirjamthomet@gmail.com>
2020-04-14 11:05:10 +0200

    add combinationpicker to game vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,19 +3,22 @@
     <Lobby v-if="game.state == 'lobby'" :game="game" />
     <Vote v-if="game.state == 'vote'" />
     <Play v-if="game.state == 'play'" :game="game" />
+    <CombinationPicker v-if="game.state == 'combination'" :game="game" />
   </div>
 </template>
 <script>
 import Lobby from "@/components/Lobby.vue";
 import Vote from "@/components/Vote.vue";
 import Play from "@/components/Play.vue";
+import CombinationPicker from "@/components/CombinationPicker.vue";
 
 export default {
   name: "Game",
   components: {
     Lobby,
     Vote,
-    Play
+    Play,
+    CombinationPicker
   },
   data: function() {
     return {
