57675f3bc78eba4e0be7296fbebaab3c44b8b7d8
John Gannon <johgannon@gmail.com>
2020-04-17 11:26:02 +0200

    Fix invalid offset on drawingpanel

--- src/components/DrawingPanel.vue
+++ src/components/DrawingPanel.vue
@@ -77,10 +77,13 @@ export default {
     currentMouse: function() {
       var c = document.getElementById("canvas");
       var rect = c.getBoundingClientRect();
+      var sx = c.scrollWidth / c.width;
+      var sy = c.scrollHeight / c.height;
+
 
       return {
-        x: this.mouse.current.x - rect.left,
-        y: this.mouse.current.y - rect.top
+        x: (this.mouse.current.x - rect.left) / sx,
+        y: (this.mouse.current.y - rect.top) / sy
       };
     }
   },
@@ -199,6 +202,5 @@ export default {
 #canvas {
   border: 1px solid blue;
   width: 100%;
-  height: 90%;
 }
 </style>
