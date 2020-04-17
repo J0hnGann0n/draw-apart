85c5ff2d023bdd6b556ed3157726efca3463f520
mirjam <mirjamthomet@gmail.com>
2020-04-17 07:57:22 +0200

    clean up user

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="row">
-    <div class="col-2">
-      <font-awesome-icon v-if="isHost" icon="crown" />
-      <font-awesome-icon v-else icon="user-tie" />
-    </div>
-    <div class="col-10">
-      <p>{{name}}</p>
+  <div class="row justify-content-center">
+    <div class="col-auto">
+      <p class="text-left">
+        <font-awesome-icon v-if="isHost" icon="crown" />
+        <font-awesome-icon v-else icon="user-tie" />
+        {{name}}
+      </p>
     </div>
   </div>
 </template>
