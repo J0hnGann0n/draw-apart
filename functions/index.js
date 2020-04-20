5e515b4a3e04ee60507c45e864526155355021df
John Gannon <johgannon@gmail.com>
2020-04-20 10:39:56 +0200

    Add back startTime for other game states

--- functions/index.js
+++ functions/index.js
@@ -141,14 +141,18 @@ exports.setState = functions.database.ref('/games/{gameKey}')
     const playersFinishedCombination = game.combinations ? Object.keys(game.combinations).length : 0
     const playersFinishedVoting = game.votes ? Object.keys(game.votes).length : 0
     const players = Object.keys(game.players).length
-    if (game.state === "drawing" && previousGameState === "lobby") {
-      ref.child('games/' + gameKey + '/countDown/startTime').set(startTime);
-    } else if (game.state === "drawing" && players === playersFinishedDrawing) {
+    let date = new Date();
+    let startTime = date.getTime();
+
+    if (game.state === "drawing" && players === playersFinishedDrawing) {
       ref.child('games/' + gameKey).child("state").set("combination");
+      ref.child('games/' + gameKey + '/countDown/startTime').set(startTime);
     } else if (game.state === "combination" && players === playersFinishedCombination) {
       ref.child('games/' + gameKey).child("state").set("voting");
+      ref.child('games/' + gameKey + '/countDown/startTime').set(startTime);
     } else if (game.state === "voting" && players === playersFinishedVoting) {
       ref.child('games/' + gameKey).child("state").set("winner");
+      ref.child('games/' + gameKey + '/countDown/startTime').set(startTime);
     }
     return true
   });
