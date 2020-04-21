45fccd5f9cd9d536c96de427cf37211625d6e389
John Gannon <johgannon@gmail.com>
2020-04-21 19:06:03 +0200

    Add sentry to track js errors

--- src/main.js
+++ src/main.js
@@ -14,6 +14,22 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import 'inobounce'
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+import Vue2TouchEvents from "vue2-touch-events";
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Sentry.init({
+  dsn: 'https://9a85c2e8b9af45bc9f38dcee787ed8be@o381283.ingest.sentry.io/5208398',
+  integrations: [new VueIntegration({ Vue, attachProps: true })],
+});
 
 library.add(
   faUser,
@@ -27,16 +43,6 @@ library.add(
   faSlidersH,
   faQuestionCircle
 )
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-import Vue2TouchEvents from "vue2-touch-events";
 
 Vue.prototype.$http = axios
 var SocialSharing = require('vue-social-sharing');
