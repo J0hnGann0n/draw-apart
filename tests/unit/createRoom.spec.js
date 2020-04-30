d14c47b4c823207cc202e9858eceb44612f96751
mirjam <mirjamthomet@gmail.com>
2020-04-30 18:09:17 +0200

    add test spec for createRoom component

---
+++ tests/unit/createRoom.spec.js
@@ -0,0 +1,36 @@
+import { mount } from "@vue/test-utils"
+import createRoom from '@/components/createRoom.vue'
+
+
+describe('createRoom.vue', () => {
+  let mockStore;
+  let wrapper;
+  beforeEach(() => {
+    mockStore = {
+      state: {
+        player: {
+          name: 'playerName'
+        }
+      },
+      dispatch: jest.fn(),
+      getters: {}
+    }
+
+    wrapper = mount(createRoom, {
+      computed: {},
+      mocks: {
+        $store: mockStore
+      }
+    })
+  })
+
+  it('dispatches createGame with player name from store state', async () => {
+
+    const button = wrapper.find('button')
+    button.trigger('click')
+
+    expect(mockStore.dispatch).toHaveBeenCalledWith(
+      "createGame", "playerName")
+
+  })
+})
