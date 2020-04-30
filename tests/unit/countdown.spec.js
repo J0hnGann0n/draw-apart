b3ec476eaaa37f266c0db95d6626d2fc8a38015f
John Gannon <johgannon@gmail.com>
2020-04-30 16:05:32 +0200

    Add unit test for countdown.vue

---
+++ tests/unit/countdown.spec.js
@@ -0,0 +1,44 @@
+import Vuex from "vuex"
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Countdown from '@/components/Countdown.vue'
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+describe('Countdown.vue', () => {
+  it('dispatches startCountDown when startCountDOwn method is run', async () => {
+    const mockStore = {
+      dispatch: jest.fn(),
+    }
+    const wrapper = shallowMount(Countdown, {
+      computed: {
+        startTime() {
+          return ""
+        },
+        game() {
+          return {
+            state: "drawing",
+            countDown: {
+              drawing: 2,
+              combination: 40,
+              voting: 30,
+              startTime: new Date().getTime()
+            }
+          }
+        }
+      },
+      mocks: {
+        $store: mockStore
+      }
+    })
+    jest.useFakeTimers()
+    let date = new Date()
+    let newStartTime = date.getTime()
+    await wrapper.vm.startCountdown(newStartTime)
+
+    expect(mockStore.dispatch).toHaveBeenCalledWith("startCountdown")
+    expect(wrapper.vm.timeleft).toEqual(2)
+    jest.runOnlyPendingTimers() // Allow jests fake timer to pass time and then test the setInterval function
+    expect(wrapper.vm.timeleft).toEqual(1)
+  })
+})
